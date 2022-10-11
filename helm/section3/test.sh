80abb965d27478f869d030dee45faf1d2fc2a412
gomip <rlawjdgns15@hotmail.com>
2022-10-11 23:00:50 +0900

    helm assignment1

--- helm/section3/test.sh
+++ helm/section3/test.sh
@@ -1,11 +1,17 @@
+# 1. helm install tomcat
 helm install tomcat bitnami/tomcat
 
-getPassword
-upgrade custom value.yaml
+# 2. get Password : ozKlYK79yE
+echo Password: $(kubectl get secret --namespace default tomcat -o jsonpath="{.data.tomcat-password}" | base64 -d)
 
-tomcatPassword: 
-service:
-  type: NodePort
-  nodePort: 30007
+# 3. Upgrade password and service
+# values.yaml
+# tomcatPassword: ozKlYK79yE
+# service:
+#   type: NodePort
+#   nodePorts:
+#     http: 31111
+helm upgrade tomcat bitnami/tomcat --values ./values.yaml
 
-helm uninstall
+# 4. delete
+helm uninstall tomcat
